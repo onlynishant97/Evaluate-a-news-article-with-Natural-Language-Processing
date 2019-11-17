bc139f8268d2aa5b161c008a84c8c9670d8e8377
onlynishant1997 <onlynishant1997@gmail.com>
2019-11-17 13:40:43 +0530

    Evaluate NLP

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Evaluate-a-news-article-with-Natural-Language-Processing
 Evaluate a news article with Natural Language Processing of Front End Developer Nanodegree on Udacity.
+
+To Start Run the Project :-
+Build prod using $ npm run build-prod
+Run $ npm start
+Wee home page at localhost:8081
---
+++ src/client/js/formHandler.js
@@ -0,0 +1,36 @@
+function handleSubmit() {
+    let result = false; 
+    let formText = getValueFromForm();
+
+    let flag = Client.newsTextValidator(formText);
+    if (flag) {
+        result = true;
+        let newsPromise = getNewsLabel(formText);
+        newsPromise().then((data) => {
+            document.getElementById('results').innerHTML = data.categories[0].label;
+        });
+    }else{
+        result = false;
+        alert("News Text Length Should Be More Than 10");
+    }
+    return result;
+}
+
+function getValueFromForm(){
+    return document.getElementById('text').value;
+}
+
+function getNewsLabel(newsText) {
+    const getData = async () => {
+        const res = await fetch(`http://localhost:8081/api?input=${newsText}`);
+        try {
+            const data = await res.json();
+            return data;
+        } catch (error) {
+            alert("Oops Something Went Wrong With The Server...");
+        }
+    }
+    return getData;
+}
+
+export { handleSubmit, getValueFromForm}
